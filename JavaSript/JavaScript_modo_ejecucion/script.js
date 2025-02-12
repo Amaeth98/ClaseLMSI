c707c067a932c684191ca162d20bd7ef42a68f12
Amaeth98 <sniukis98@gmail.com>
2025-02-12 10:59:06 +0100

    12/02/2025

---
+++ JavaSript/JavaScript_modo_ejecucion/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+let titulo2 = document.querySelector("h2");
+  titulo2.addEventListener("click", function() {
+    alert("¡Click en h2!");
+    console.log("hola");
+});
+
+
+});
