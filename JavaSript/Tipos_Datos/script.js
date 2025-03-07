514be94116196e15c7dd826ba83cf78299b6a16d
Amaeth98 <sniukis98@gmail.com>
2025-03-07 12:32:12 +0100

    05/03/2025

--- JavaSript/Tipos_Datos/script.js
+++ JavaSript/Tipos_Datos/script.js
@@ -74,8 +74,11 @@ var persona1 = {
   /: Delimitadores de la expresión regular.
   java: Coincide literalmente con la cadena "java".
   (script)?: Grupo de captura opcional. Puede coincidir con "script" cero o una vez.
-  i: Modificador de la expresión regular que hace que la coincidencia sea insensible a mayúsculas y minúsculas.
-  Por lo tanto, esta expresión regular buscará "java" seguido opcionalmente por "script" en una cadena de texto, y la búsqueda no será sensible a mayúsculas y minúsculas. Esto significa que coincidirá con "java", "JavaScript" o "javaSCRIPT", entre otras posibles variaciones.*/
+  i: Modificador de la expresión regular que hace que la coincidencia sea insensible 
+  a mayúsculas y minúsculas.
+  Por lo tanto, esta expresión regular buscará "java" seguido opcionalmente por 
+  "script" en una cadena de texto, y la búsqueda no será sensible a mayúsculas y minúsculas. 
+  Esto significa que coincidirá con "java", "JavaScript" o "javaSCRIPT", entre otras posibles variaciones.*/
   var patron = /java(script)?/i;
   
   console.log("Expresión Regular:", patron);
